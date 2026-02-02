ce7b881ec2b068fe488a019d6e76d2e395e1f4b6
unknown <anhelina.pryadko@gmail.com>
2026-02-02 13:59:04 +0200

    fix classname error

--- CinemaWeb/ViewModels/FilmFormViewModel.cs
+++ CinemaWeb/ViewModels/FilmFormViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CinemaWeb.ViewModels
 {
-    public class AdminFilmFormViewModel
+    public class FilmFormViewModel
     {
         public int Id { get; set; }
 
